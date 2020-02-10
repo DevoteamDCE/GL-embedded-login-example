943bdcaf1f0d579fefe81028d0baeac95e8b9511
Rupert Barrow <AltiusRupert@users.noreply.github.com>
2020-02-10 11:37:05 +0100

    Added other env variables
    SALESFORCE_EXPID

--- index.php
+++ index.php
@@ -23,7 +23,8 @@
     <meta name="salesforce-logout-handler" content="onLogout">
     <meta name="salesforce-mask-redirects" content="<?php echo getenv('SALESFORCE_MASK_REDIRECTS');?>">
     <meta name="salesforce-cache-max-age" content="10">
-
+	<meta name="salesforce-expid" content="<?php echo getenv('SALESFORCE_EXPID');?>">
+	  
 	<link  href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
     <script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget<?php echo getenv('SALESFORCE_LOGINWIDGETCONTROLLER_PARAMS');?>" async defer></script>
   </head>
