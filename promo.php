24dd16bdd8fde70841183ec19ad27d8a459b93e4
Rupert Barrow <AltiusRupert@users.noreply.github.com>
2020-02-19 23:14:18 +0100

    Hide login link

--- promo.php
+++ promo.php
@@ -30,7 +30,7 @@
   </head>
   
   <body>
-  	<div id="sign-in-link" style="position: absolute; top: 20px;right: 20px;"></div>
+  	<!--<div id="sign-in-link" style="position: absolute; top: 20px;right: 20px;"></div>-->
     <header>
       <div class="masthead-elements-row-1">
         <div class="element-left"></div>
